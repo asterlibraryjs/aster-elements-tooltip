490f1661f10661302bc5a40ebef12154dd22d12e
Sébastien Doucet <sebdoucet@gmail.com>
2022-01-31 13:48:12 -0500

    Avoid resolve nested dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,7 @@
 import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
 import sourcemap from "rollup-plugin-sourcemaps";
 import css from "rollup-plugin-import-css";
 import copy from "rollup-plugin-copy";
-import pkg from "./package.json";
 
 export default [
     {
@@ -19,7 +17,6 @@ export default [
         plugins: [
             css(),
             commonjs(),
-            resolve(),
             sourcemap(),
             copy({
                 hook: "buildStart",
@@ -27,9 +24,6 @@ export default [
                     { src: "src/**/*.css", dest: "lib/src/" }
                 ]
             })
-        ],
-        external: [
-            ...Object.keys({ ...pkg.devDependencies, ...pkg.dependencies })
         ]
     }
 ];
